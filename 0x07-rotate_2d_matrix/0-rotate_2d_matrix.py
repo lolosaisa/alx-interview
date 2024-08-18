fa528c4a767ec5af8cffcd46a8441186fbc635df
lolosaisa <lwamaitha3924@gmail.com>
2024-08-18 14:02:22 -0700

    functon rotates 2d matrix with 90 degrees

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Rotate 2D Matrix
+Rotares 90 degrees
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """Fanction rotates two dimension matrix 90 degrees clockwise
+    Args:
+        matrix (list[[list]]): a matrix
+    """
+    n = len(matrix)
+    for i in range(int(n / 2)):
+        y = (n - i - 1)
+        for j in range(i, y):
+            x = (n - 1 - j)
+            # current number
+            tmp = matrix[i][j]
+            # change top for left
+            matrix[i][j] = matrix[x][i]
+            # change left for bottom
+            matrix[x][i] = matrix[y][x]
+            # change bottom for right
+            matrix[y][x] = matrix[j][y]
+            # change right for top
+            matrix[j][y] = tmp
