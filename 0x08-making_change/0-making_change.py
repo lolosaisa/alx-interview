72a26f9159099bef4207bc2bd08eca16b5c81798
lolosaisa <lwamaitha3924@gmail.com>
2024-08-21 18:30:21 +0300

    uses bottom up method to find the fewest number of total

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
 
-
-
 def makeChange(coins, total):
-    memo = {}
-
-    #there is only one way to find no of coins for total 0
-    memo[0] = 1
-
-    for i in range(1, total+1):
-        memo[1] = 0
-
-        #check the solution of previous coin by checking each coin denomination
+    #using bottom up, initially our array can hold the largest possible value
+    memo = [float(inf)]* (total+1)
+    
+    #we need 0 coins to make the total of 0
+    memo[0] = 0
+    
+    #we loop through each amount from 1 to total
+    for i in range(1, total +1):
+        #we ae going to chechk the previous solution/ coin denomination
         for coin in coins:
-            subproblem = i - coin
-
-            #if your solution is negative, meaning we cant use the coin but if it is positive we can use it
-            if subproblem >= 0:
-                memo[i] += memo[subproblem]
-
- return memo[total]
-
-
+            if i - coin >= 0:
+                memo[1] = min(memo[i], memo[i-coin] + 1)
+                
+    #if memo[total] is infinity it means it is an impossible task            
+    return memo[total] if memo[total] != float('inf')else -1
