57ff763499721b690d9b5cda8955ce99b83b84b8
lolosaisa <lwamaitha3924@gmail.com>
2024-08-30 04:35:32 +0300

    changed the logic to fix requirement fail

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
+"""Defines island perimeter finding function."""
 
-"""Function calculates the perimeter of an island"""
 
 def island_perimeter(grid):
-    perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0])
-    
-    for i in range(rows):
-        for j in range(cols):
+    """Return the perimiter of an island.
+    The grid represents water by 0 and land by 1.
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                # assuming all land cells have four sides
-                perimeter += 4
-                
-                # check the neighbor cells and subtract shared sides
-                if i > 0 and grid[i - 1][j] == 1:  # Check the cell above
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:  # Check the cell to the left
-                    perimeter -= 2
-    
-    return perimeter
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
