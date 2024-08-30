727042bfee58238e9e1c5f0d99b46e8209c54d86
lolosaisa <lwamaitha3924@gmail.com>
2024-08-30 04:15:24 +0300

    function calculates the perimeter of an island

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,22 @@
+#!usr/bin/python3
+
+"""Function calculates the perimeter of an island"""
+
+def island_perimeter(grid):
+    perimeter = 0;
+    rows = len(grid),
+    cols = len(grid[0])
+    
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                #assuming all land cells have four sides
+                perimeter += 4
+                
+                #we check the neigbour cells and substract shared sides
+                if i > 0 and grid[i - j][j] == 1:
+                    perimeter -= 2
+                if j> 0 grid[i][j - 1] == 1:
+                    perimeter -= 2
+                        
+            return perimeter
